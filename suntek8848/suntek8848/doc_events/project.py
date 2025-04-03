e80b1c8f3e3a357ac5e818aef6032e889338b9f4
shubham-DT <shubham@8848digital.com>
2025-04-03 17:00:24 +0530

    fix: added code to set state in project if to channel partner checked

--- suntek8848/suntek8848/doc_events/project.py
+++ suntek8848/suntek8848/doc_events/project.py
@@ -4,7 +4,10 @@ import frappe
 def fetch_sales_order_fields(doc, method):
     if doc.sales_order:
         so = frappe.get_doc("Sales Order", doc.sales_order)
-        doc.custom_state = so.custom_suntek_state
+        if so.state:
+            doc.custom_state = so.state
+        else:
+            doc.custom_state = so.custom_suntek_state
         doc.custom_branch = so.branch
 
         doc.save()
